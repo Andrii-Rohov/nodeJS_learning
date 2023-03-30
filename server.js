dd209a2345d129f95c1aa1d5742c45d08c85fcb0
Andrii Rohov <andrik.la.resistance@gmail.com>
2023-03-30 16:05:56 +0300

    8_section: Completed section commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on("uncaughtException", (err) => {
+    //Uncaught Exeprions - problems in sync code a.k.a typos, non declare vars etc
+    console.error(err.name, err.message);
+    process.exit(1);
+})
+
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
@@ -18,6 +24,14 @@ const port = process.env.PORT || 8000;
 // console.log(process.env);
 // console.log(app.get('env'));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App runing on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+    //Unhandled Rejection - problems in async code a.k.a Promises, fetch() etc
+    console.error(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 })
